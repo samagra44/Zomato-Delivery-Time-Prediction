ae3c20c3164f525521ebb0e506cd16b27492f896
samagra44 <samagra44@gmail.com>
2023-09-06 11:37:30 +0530

    Data Transformation completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.logger import logging
 from src.exception import CustomException
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig():
@@ -39,3 +40,5 @@ class DataIngestion:
 if __name__ =='__main__':
     obj = DataIngestion()
     train_data,test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_ = data_transformation.inititate_data_transformation(train_data,test_data)
